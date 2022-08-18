ebc3db867ff2cd9879471c72295d358bd3d03a27
Sevtap Fernengel <92039320+sfernengel@users.noreply.github.com>
2022-08-18 12:29:55 +0200

    Initial commit

---
+++ custom-application-config.mjs
@@ -0,0 +1,43 @@
+import { PERMISSIONS, entryPointUriPath } from './src/constants';
+
+/**
+ * @type {import('@commercetools-frontend/application-config').ConfigOptions}
+ */
+const config = {
+  name: 'Training Custom App',
+  entryPointUriPath,
+  cloudIdentifier: 'gcp-eu',
+  env: {
+    development: {
+      initialProjectKey: 'sunrise-0519-poc',
+    },
+
+    production: {
+      applicationId: 'TODO',
+      url: 'https://your_app_hostname.com',
+    },
+  },
+
+  oAuthScopes: {
+    view: ['view_products'],
+    manage: ['manage_products'],
+  },
+
+  icon: '${path:@commercetools-frontend/assets/application-icons/rocket.svg}',
+  mainMenuLink: {
+    defaultLabel: 'Template starter',
+    labelAllLocales: [],
+    permissions: [PERMISSIONS.View],
+  },
+
+  submenuLinks: [
+    {
+      uriPath: 'channels',
+      defaultLabel: 'Channels',
+      labelAllLocales: [],
+      permissions: [PERMISSIONS.View],
+    },
+  ],
+};
+
+export default config;
